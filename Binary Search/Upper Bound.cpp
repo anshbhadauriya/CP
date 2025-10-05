3300a7020648b05006a9b41f46e096a190c2dd20
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-10-05 20:57:28 +0530

    Update Upper Bound.cpp

--- Binary Search/Upper Bound.cpp
+++ Binary Search/Upper Bound.cpp
@@ -3,12 +3,13 @@ int upperBound(const vector<int>& nums, int target) {
     int ans = nums.size();  // default: if target >= all elements
     while (l <= h) {
         int mid = l + (h - l) / 2;
-        if (nums[mid] <= target) {
+        if (nums[mid] > target) {
+            ans = mid;      // potential upper bound
             l = mid + 1;    // search right half
         } else {
-            ans = mid;      // potential upper bound
             h = mid - 1;    // search left half
         }
     }
     return ans;
+
 }
