6f9f33d7c853778ad75d6f24751363dd861b72aa
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-10-05 21:05:19 +0530

    Update Lower Bound.cpp

--- Binary Search/Lower Bound.cpp
+++ Binary Search/Lower Bound.cpp
@@ -1,14 +1,23 @@
 int lowerBound(const vector<int>& nums, int target) {
     int l = 0, h = nums.size() - 1;
-    int ans = nums.size();  // default: if target > all elements
+    int ans = nums.size();
     while (l <= h) {
         int mid = l + (h - l) / 2;
         if (nums[mid] >= target) {
-            ans = mid;      // potential lower bound
-            h = mid - 1;    // search left half
+            ans = mid; 
+            h = mid - 1;
         } else {
-            l = mid + 1;    // search right half
+            l = mid + 1;
         }
     }
     return ans;
 }
+
+c++ me inbuild function bhi use kar skte ho 
+for example-
+    
+vector<int> nums = {1, 3, 3, 5, 7};
+    int target = 3;
+
+    auto it = lower_bound(nums.begin(), nums.end(), target);
+    int index = it - nums.begin();  // convert iterator to index
