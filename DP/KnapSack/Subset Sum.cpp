8a0d4d4b32b2bf247f179c96548748c0b9e7c508
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-10-08 15:59:24 +0530

    Update Subset Sum.cpp

--- DP/KnapSack/Subset Sum.cpp
+++ DP/KnapSack/Subset Sum.cpp
@@ -1,3 +1,6 @@
+Recusrive->
+
+
 class Solution {
   public:
   bool solve(vector<int>&arr,int sum,int i){
@@ -15,6 +18,37 @@ class Solution {
   }
     bool isSubsetSum(vector<int>& arr, int sum) {
        return solve(arr,sum,0);
+        
+    }
+
+};
+
+
+
+
+Memoized->
+
+  class Solution {
+  public:
+  bool solve(vector<int>&arr,int sum,int i,vector<vector<int>>&dp){
+      if(sum==0){
+          return true;
+      }
+      if(dp[i][sum]!=-1){
+          return dp[i][sum];
+      }
+       if(i==arr.size()) return false; 
+       
+      if(arr[i]<=sum){
+          return dp[i][sum]=solve(arr,sum-arr[i],i+1,dp) || solve(arr,sum,i+1,dp);
+      }
+      else {
+            return dp[i][sum]=solve(arr,sum,i+1,dp);
+        }
+  }
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        vector<vector<int>> dp(arr.size(), vector<int>(sum + 1, -1));
+       return solve(arr,sum,0,dp);
         
     }
 };
