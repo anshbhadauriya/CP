66ae432a844e40749e1c51dec8cb07e9b3a8e1f7
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-10-12 21:47:09 +0530

    Create Print Longest common subsequence.cpp

---
+++ DP/LCS/Print Longest common subsequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string solve(string &text1, string &text2, int i, int j, vector<vector<string>> &dp) {
+        if(i==0 || j==0) {
+          return ""; 
+    }
+
+        if(dp[i][j]!=""){
+          return dp[i][j]; 
+        }
+
+        if(text1[i-1]==text2[j-1]) {
+            dp[i][j]=solve(text1, text2, i-1, j-1, dp)+text1[i-1];
+        } else {
+            string left = solve(text1, text2, i-1, j,dp);
+            string right = solve(text1, text2, i, j-1,dp);
+            if(left.size() > right.size()) {
+                dp[i][j] = left;
+            } else {
+                dp[i][j] = right;
+            }
+        }
+        return dp[i][j];
+    }
+
+    string longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<string>> dp(n+1, vector<string>(m+1, ""));
+        return solve(text1, text2, n, m, dp);
+    }
+};
