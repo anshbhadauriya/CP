1ff34dcfb75f8fe89d019bdbf21070d87453bd36
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-10-13 23:40:54 +0530

    Update Print Longest common subsequence.cpp

--- DP/LCS/Print Longest common subsequence.cpp
+++ DP/LCS/Print Longest common subsequence.cpp
@@ -2,24 +2,27 @@ Tabulation->
 
     Step 1-> pehle Longest Common Subsequence wala code likho
     Step 2-> Backtrack to find the actual subsequence
-   int i = m, j = n;
-        string lcs = "";
-        while (i > 0 && j > 0) {
-            if (text2[i - 1] == text1[j - 1]) {
-                lcs += text1[j - 1]; // add matching character
-                i--;
-                j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+    string scs = "";
+        int i = n, j = m;
+        while(i > 0 && j > 0) {
+            if(text1[i-1] == text2[j-1]) {
+                scs += text1[i-1];
+                i--; j--;
+            } else if(dp[i-1][j] > dp[i][j-1]) {
+                scs += text1[i-1];
                 i--;
             } else {
+                scs += text2[j-1];
                 j--;
             }
         }
 
-        reverse(lcs.begin(), lcs.end()); // correct order
-        return lcs; // return the actual LCS
+        while(i > 0) scs += text1[i-1], i--;
+        while(j > 0) scs += text2[j-1], j--;
+
+        reverse(scs.begin(), scs.end());
+        return scs;
     }
-};
 
 
 
