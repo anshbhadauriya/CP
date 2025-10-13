f9b29a6459d76026f63e2b9220ce0176911427b4
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-10-13 23:11:16 +0530

    Update Longest Common Subsequence.cpp

--- DP/LCS/Longest Common Subsequence.cpp
+++ DP/LCS/Longest Common Subsequence.cpp
@@ -53,34 +53,28 @@ int solve(string text1,string text2,int i,int j,vector<vector<int>>&dp){
 
 Tabulation->
 
+    
     class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size();
-        int m=text2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1));
-        for(int i=0;i<m+1;i++){
-            for(int j=0;j<n+1;j++){
-                if(i==0 or j==0){
-                    dp[i][j]=0;
-                }
-            }
-        }
-        for(int i=1;i<m+1;i++){
-            for(int j=1;j<n+1;j++){
-                if(text1[j-1]==text2[i-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+         int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text2[i - 1] == text1[j - 1]){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-
         return dp[m][n];
-
     }
 };
+
 
 
 
