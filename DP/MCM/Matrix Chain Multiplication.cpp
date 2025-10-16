0dc74bcfad4432fa14950ea026604a9c6f89ab43
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-10-16 22:52:32 +0530

    Update Matrix Chain Multiplication.cpp

--- DP/MCM/Matrix Chain Multiplication.cpp
+++ DP/MCM/Matrix Chain Multiplication.cpp
@@ -1,3 +1,5 @@
+Recursive->
+
 int MCM(int i, int j, vector<int>& arr) {
 
     if (i == j) return 0;
@@ -13,3 +15,26 @@ int MCM(int i, int j, vector<int>& arr) {
 
     return mini;
 }
+
+Memoized->
+
+    int MCM(int i, int j, vector<int>& arr, vector<vector<int>>& dp) {
+    if (i == j) return 0;
+
+    if (dp[i][j] != -1) return dp[i][j]; 
+
+    int mini = INT_MAX;
+    for (int k = i; k < j; k++) {
+        int cost = MCM(i, k, arr, dp)
+                 + MCM(k + 1, j, arr, dp)
+                 + arr[i - 1] * arr[k] * arr[j];
+
+        mini = min(mini, cost);
+    }
+
+    return dp[i][j] = mini; 
+}
+
+
+    
+
