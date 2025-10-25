3a4ec6479972d8e6e738b59b9b8df8f7ef52434f
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-10-25 23:45:16 +0530

    Create Biweekly 168.cpp

---
+++ Leetcode/Contest/Biweekly 168.cpp
@@ -0,0 +1,55 @@
+Q1->
+class Solution {
+public:
+    string lexSmallest(string s) {
+        string minimum=s,minimum2=s;
+        int idx,idx2;
+      for(int i=0;i<s.size();i++){
+          string temp=s;
+         
+          reverse(temp.begin(),temp.begin()+i+1);
+          if(temp<minimum){
+          minimum=temp;
+              idx=i;
+          }
+      }
+
+
+        for(int i=s.size()-1;i>=0;i--){
+            string temp=s;
+           
+            reverse(temp.begin()+i,temp.end());
+            if(temp<minimum2){
+                minimum2=temp;
+                idx2=i;
+            }
+        }
+        if(minimum<minimum2){
+            reverse(s.begin(),s.begin()+idx+1);
+            return s;
+        }
+        reverse(s.begin()+idx2,s.end());
+        return s;
+        
+        
+    }
+
+Q2->
+
+class Solution {
+public:
+    string maxSumOfSquares(int num, int sum) {
+       string total="";
+       for(int i=0;i<num;i++){
+        int x=min(9,sum);
+        total+=x+'0';
+        sum=sum-x;
+       }
+       if(sum>0){
+        return "";
+       }
+       return total;
+        
+    }
+};
+};
