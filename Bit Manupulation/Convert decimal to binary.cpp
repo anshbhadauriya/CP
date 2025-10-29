d27c9dbad033e8b9a7ecdea7e84bd8d11d42cdda
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-10-29 13:59:29 +0530

    Update Convert decimal to binary.cpp

--- Bit Manupulation/Convert decimal to binary.cpp
+++ Bit Manupulation/Convert decimal to binary.cpp
@@ -1,4 +1,4 @@
- string binary="";
+            string binary="";
             int num=i;
 
             while(num>0) {
@@ -6,3 +6,4 @@
                 binary=to_string(remainder)+binary;
                 num=num/2;
             }
+
