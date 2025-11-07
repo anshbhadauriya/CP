0cfe4d1b0478ff722dc093f5db64849af232ccd9
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-11-07 12:20:57 +0530

    Add files via upload

---
+++ CodeForces/800 rated/Maple and Multiplication.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+	int t;
+	cin>>t;
+	while(t--){
+		int a,b;
+		cin>>a>>b;
+		if(a==b){
+			cout<<0<<endl;
+			continue;
+			
+		}
+		else if(a%b==0 or b%a==0){
+			cout<<1<<endl;
+			continue;
+		}
+		else{
+			cout<<2<<endl;
+			continue;
+		}
+		
+		
+	}
+
+
+
+    return 0;
+}
