6d3cafcc13cb7b4cf136ab568af899fa61fc7e0d
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-11-10 21:13:18 +0530

    Add files via upload

---
+++ CodeForces/Round 1063 (Div. 2)/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        vector<int>nums(n);
+        for(int i=0;i<n;i++) {
+            cin>>nums[i];
+        }
+        
+        sort(nums.begin(),nums.end());
+        
+        bool can_win=true;
+        for (int i=1;i<n-1;i+=2) {
+            if (nums[i]!=nums[i + 1]) {
+                can_win=false;
+                break;
+            }
+        }
+        
+        if(can_win){
+            cout<<"YES"<<endl;
+        } else {
+            cout<<"NO"<<endl;
+        }
+    }
+    
+    return 0;
+}
