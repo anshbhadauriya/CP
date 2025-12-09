f89a08f5cd8c37567394bd9fcb59b4da80778283
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-12-09 19:18:03 +0530

    Update Upper Bound.cpp

--- Binary Search/Upper Bound.cpp
+++ Binary Search/Upper Bound.cpp
@@ -5,9 +5,9 @@ int upperBound(const vector<int>& nums, int target) {
         int mid = l + (h - l) / 2;
         if (nums[mid] > target) {
             ans = mid; 
-            l = mid + 1;
-        } else {
             h = mid - 1;
+        } else {
+            l = mid + 1;
         }
     }
     return ans;
@@ -24,4 +24,5 @@ vector<int> nums = {1, 3, 3, 5, 7};
     int index = it - nums.begin();  // convert iterator to index
 
     cout << "Upper bound index: " << index << endl; // 3
+
 
