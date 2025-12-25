35f4a67ccc759c74aeaf0f05b1239cfc125896de
anshbhadauriya <2k23.csai2310627@gmail.com>
2025-12-25 18:58:17 +0530

    Update circular array tricks.cpp

--- Algo/circular array tricks.cpp
+++ Algo/circular array tricks.cpp
@@ -1,1 +1,9 @@
+Kabhi bhi circulary array ho too har bar yeh nhi krna ki agr i=nums.size() too i=0 
+yaa fir i=0 so i=nums.size()-1 
+
+  instead do this-->
+int safeindex(int i , int n){
+    if(i<0)return n-1;
+    return i%n;
+}
 
