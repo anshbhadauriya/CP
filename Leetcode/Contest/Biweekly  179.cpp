5d9e3c31a9ed0fbde33fe1ab137d52d8c48e83f1
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-03-28 21:05:19 +0530

    Create Biweekly  179.cpp

---
+++ Leetcode/Contest/Biweekly  179.cpp
@@ -0,0 +1,55 @@
+Q1->
+  class Solution {
+public:
+    int minAbsoluteDifference(vector<int>& nums) {
+        if(nums.size()<2) return -1;
+        int minimum=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+               
+                if(nums[i]==1 and nums[j]==2){
+                minimum=min(minimum,abs(i-j));
+                }
+            }
+        }
+        if(minimum==INT_MAX) return -1;
+        return minimum;
+        
+    }
+};
+
+Q3->
+  class Solution {
+public:
+    int n,m;
+    
+
+    int solve(int i,int j,int xorr,vector<vector<int>>&grid,vector<vector<vector<int>>>&dp){
+        if(i==n-1 and j==m-1){
+            return xorr^grid[i][j];
+        }
+        if(dp[i][j][xorr]!=-1) return dp[i][j][xorr];
+
+        int down=INT_MAX,right=INT_MAX;
+
+        if(i+1<n){
+            down=solve(i+1,j,xorr^grid[i][j],grid,dp);
+        }
+        if(j+1<m){
+            right=solve(i,j+1,xorr^grid[i][j],grid,dp);
+        }
+
+        return dp[i][j][xorr]=min(down,right);
+
+        
+        
+    }
+    
+    int minCost(vector<vector<int>>& grid) {
+
+        n=grid.size(),m=grid[0].size();
+vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(1024,-1)));
+        return solve(0,0,0,grid,dp);
+        
+    }
+};©leetcode
