72d25bd9ce33792f6201ec54f5c20d9fa87cf8c6
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-04-11 02:07:46 +0530

    Create can we sort array after some rotations? .cpp

---
+++ can we sort array after some rotations? .cpp
@@ -0,0 +1,11 @@
+if there is an array and you want to know that after some left or right rotation array can be sorted or not so 
+
+if(arr[i]>arr[i+1]) drops++;
+
+
+
+if(drops==1) so array can be sorted after rotations
+
+if(drops==0) array is already sorted
+
+if(drops>1) array can not be sorted after rotations
