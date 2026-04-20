2140ad2cac46b508272f3abaf0936c47a48b3dcb
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-04-20 23:48:37 +0530

    Create Linked List Cycle II.cpp

---
+++ LinkedList/Linked List Cycle II.cpp
@@ -0,0 +1,21 @@
+With space->
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+        unordered_set<ListNode*>st;
+
+        ListNode* temp=head;
+
+        while(temp!=NULL){
+            if(st.count(temp)) return temp;
+
+            st.insert(temp);
+            temp=temp->next;
+        }
+
+        return NULL;
+        
+    }
+};
