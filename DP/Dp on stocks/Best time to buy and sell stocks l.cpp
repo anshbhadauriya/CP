d7d24d56078e3478f11e614293f6584bbec86558
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-05-01 20:15:40 +0530

    Update Best time to buy and sell stocks l.cpp

--- DP/Dp on stocks/Best time to buy and sell stocks l.cpp
+++ DP/Dp on stocks/Best time to buy and sell stocks l.cpp
@@ -11,3 +11,46 @@ public:
      return maximum;   
     }
 };
+
+
+Recursive->
+(not good appraoch for this ques)
+    class Solution {
+public:
+
+int solve(int i, int bought, int transactions, vector<int>& prices,vector<vector<vector<int>>>&dp){
+
+    if(i >= prices.size()) return 0;
+
+    // already done 1 transaction so stop
+    if(transactions == 1) return 0;
+
+    if(dp[i][bought][transactions]!=-1) return dp[i][bought][transactions]; 
+
+    
+
+    if(bought == 0){
+        // buy or skip
+        int buy = -prices[i] + solve(i+1, 1, transactions, prices,dp);
+        int skip = solve(i+1, 0, transactions, prices,dp);
+
+        return dp[i][bought][transactions]=max(buy, skip);
+    }
+    else{
+        // sell or hold
+        int sell = prices[i] + solve(i+1, 0, 1, prices,dp);
+        int hold = solve(i+1, 1, transactions, prices,dp);
+
+        
+        return dp[i][bought][transactions]=max(sell, hold);
+    }
+
+    
+}
+
+int maxProfit(vector<int>& prices) {
+
+    vector<vector<vector<int>>>dp(prices.size(),vector<vector<int>>(2,vector<int>(2,-1)));
+    return solve(0, 0, 0, prices,dp);
+}
+};
