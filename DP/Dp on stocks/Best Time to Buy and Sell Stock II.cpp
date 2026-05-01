b4c0d20d90def2aaf11ae75f5af72888bd5f198f
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-05-01 23:02:48 +0530

    Update Best Time to Buy and Sell Stock II.cpp

--- DP/Dp on stocks/Best Time to Buy and Sell Stock II.cpp
+++ DP/Dp on stocks/Best Time to Buy and Sell Stock II.cpp
@@ -72,3 +72,34 @@ public:
         return dp[0][1];
     }
 };
+
+
+New attempt->
+
+class Solution {
+public:
+
+int solve(int i,int bought,vector<int>&prices, vector<vector<int>>&dp){
+
+    if(i>=prices.size()) return 0;
+
+    if(dp[i][bought]!=-1) return dp[i][bought];
+
+    if(bought==0){
+        int buy=-prices[i]+solve(i+1,1,prices,dp);
+        int skip=solve(i+1,bought,prices,dp);
+
+        return dp[i][bought]= max(buy,skip);
+    }
+    else{
+        int sell=prices[i]+solve(i+1,0,prices,dp);
+        int not_sell=solve(i+1,bought,prices,dp);
+        
+        return dp[i][bought]=max(sell,not_sell);
+    }
+}
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
+        return solve(0,0,prices,dp);
+    }
+};
