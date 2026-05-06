fc7e439b50d552fd8da95d7fae675406f3deea26
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-05-06 20:35:40 +0530

    Delete Templates/Algo directory

--- Templates/Algo/Convert decimal to binary.cpp
+++
@@ -1,8 +0,0 @@
-            string binary="";
-            int num=i;
-
-            while(num>0) {
-                int remainder=num%2;
-                binary=to_string(remainder)+binary;
-                num=num/2;
-            }
