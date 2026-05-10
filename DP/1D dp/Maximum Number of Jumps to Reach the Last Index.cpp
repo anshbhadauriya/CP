dfdbcd656824df8f8a3e38de44d90d5b2ec3517d
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-05-10 21:22:44 +0530

    Create Maximum Number of Jumps to Reach the Last Index.cpp

---
+++ DP/1D dp/Maximum Number of Jumps to Reach the Last Index.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+vector<int>dp;
+
+int solve(int i,vector<int>&nums,int &target){
+
+    if(i==nums.size()-1){
+        return 0;
+    }
+
+    if(dp[i]!=-2) return dp[i];
+
+    int ans=-1;
+
+    for(int j=i+1;j<nums.size();j++){
+        if(abs(nums[j]-nums[i])<=target){
+        int temp=solve(j,nums,target);
+
+        if(temp!=-1) ans=max(ans,1+temp);
+
+        }
+    }
+
+    return dp[i]=ans;
+
+}
+    int maximumJumps(vector<int>& nums, int target) {
+
+        dp.resize(nums.size(),-2);
+
+        return solve(0,nums,target);
+        
+    }
+};
