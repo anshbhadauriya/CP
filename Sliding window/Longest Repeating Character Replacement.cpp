917d8c45fa999b161513fab333d91b52bde4eafb
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-05-28 14:14:17 +0530

    Update Longest Repeating Character Replacement.cpp

--- Sliding window/Longest Repeating Character Replacement.cpp
+++ Sliding window/Longest Repeating Character Replacement.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
-    int characterReplacement(string s, int k) {
-        unordered_map<char,int>mp;
-        int l=0,h=0,maximum=INT_MIN,max_freq=0;
-        while(h<s.size()){
-            mp[s[h]]++;
-             max_freq=max(max_freq,mp[s[h]]);
-            int length=h-l+1;
-            int operations=length-max_freq;
-            while(operations>k and l<=h){    //window size - max frequency of a character
-                mp[s[l]]--;
-                l++;
-                length=h-l+1;
-                operations=length-max_freq;
+/*
 
+tricky ques..
+
+iss question me we have to keep track of ele which has highest frequency
+
+kyuki agr kabhi koi aur ele aaya too ham max freq ele ke alawa bakio ko hataege
+
+so basically current_window_length-max_freq krke pata chlega kitne element hai subarray me jo max freq me nhi aate hai.. hame ussi elements ko remove krna hai
+
+and keep track of maximum window
+
+
+*/
+    int characterReplacement(string s, int k) {
+
+        int l=0,h=0,maximum=0,max_freq_ele=0;
+
+        unordered_map<char,int>mp;
+
+        while(h<s.size()){
+
+            mp[s[h]]++;
+
+            max_freq_ele=max(max_freq_ele,mp[s[h]]);
+
+
+            while((h-l+1)-max_freq_ele>k and l<=h){   //(h-l+1)-max_freq_ele means ki jo ele max freq na ho..and always remember ki dont make a variable for window size bcs window is changing its length so if you make a variable for window length it will be of fixed size
+
+                mp[s[l]]--;
+                
+                l++;
             }
+
             maximum=max(maximum,h-l+1);
+            
             h++;
+
         }
+
         return maximum;
+        
     }
 };
