166a175d88f3f839fb3196bbce1204d25bca85e8
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-05-31 10:42:55 +0530

    Update Code.cpp

--- Graphs/Cycle detection in directed graph (DFS)/Code.cpp
+++ Graphs/Cycle detection in directed graph (DFS)/Code.cpp
@@ -1,20 +1,50 @@
+/*
+if node is already visited on the same path than it is cyclic
+
+mtlb agr kisi path me travel kr rhe ho too uss path ko visted mark krte rho aur suppose koi node hai vo already visited hai aur vo same path me bhi aachuka hai mtlb vo same path me dusri
+baar mila mtlb that is cyclic
+*/
+
 class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int>dq;
-        vector<int>maximum;
-        for(int i=0;i<nums.size();i++){
-
-            while(!dq.empty() and nums[dq.back()]<=nums[i]) dq.pop_back();
-
-            if(!dq.empty() and dq.front()<=i-k) dq.pop_front();
-
-            dq.push_back(i);
-
-            if(i>=k-1)
-            maximum.push_back(nums[dq.front()]);
+  public:
+  
+  bool dfs(int i,vector<vector<int>>&adj,vector<bool>&visited,vector<bool>&path){
+      
+      visited[i]=true;
+      path[i]=true;
+      
+      for(auto x:adj[i]){
+          
+          if(!visited[x]){
+              
+              if(dfs(x,adj,visited,path)) return true;
+          }
+          else if(path[x]) return true;
+      }
+      
+      path[i]=false;
+      
+      return false;
+      
+  }
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        vector<vector<int>>adj(V);
+        
+        for(auto x:edges){
+            int u=x[0],v=x[1];
+            
+            adj[u].push_back(v);
         }
-
-        return maximum;
+        
+        vector<bool>visited(V,false),path(V,false);
+        
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(dfs(i,adj,visited,path)) return true;
+            }
+        }
+        
+        return false;
+        
     }
 };
