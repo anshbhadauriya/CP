c0130fac3547505cd4d40d87644b3732a45735d5
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-06-02 18:07:15 +0530

    Create Sort Colors.cpp

---
+++ Arrays/Sort Colors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+/*
+
+Dutch National Flag Algorithm 
+you cant solve without revising typaa question
+
+*/
+
+    void sortColors(vector<int>& nums) {
+
+        int low=0,mid=0,high=nums.size()-1;
+
+        while(mid<=high){
+            
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+        
+    }
+};
