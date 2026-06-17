7fe12c0ef1de13990f9abade09d19d5e7ccb4502
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-06-17 18:25:53 +0530

    Update Combination Sum.cpp

--- Backtracking/Combination Sum.cpp
+++ Backtracking/Combination Sum.cpp
@@ -1,3 +1,58 @@
+class Solution {
+public:
+/*
+Har recursive call me we have 2 options
+
+take it and stay in same idx
+
+skip and go in next idx
+
+base case will be out of bound and sum>target
+*/
+
+vector<vector<int>>result;
+
+void solve(int i,vector<int>&curr,int sum,int &target,vector<int>& candidates){
+
+    if(sum==target){
+        result.push_back(curr);
+        return;
+    }
+
+    if(sum>target) return;
+
+    if(i==candidates.size()) return;
+
+    curr.push_back(candidates[i]);
+
+    solve(i,curr,sum+candidates[i],target,candidates);
+
+    curr.pop_back();
+
+    solve(i+1,curr,sum,target,candidates);
+     
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int sum=0;
+        vector<int>curr;
+
+        solve(0,curr,sum,target,candidates);
+
+        return result;
+
+        /*
+        TC-> isme target tak subset bnege so uk agr subsets hote too 2^n hota lekin idhr subsets depends upon target and elements in array so if large ele are there only so we can reach target early so 
+        O(N^ target/min(candidate)​)
+
+        SC-> O(target/min(candidate)​)
+
+        */
+        
+    }
+};
+
+
+
 Bad for memory approach-> 
 
   class Solution {
