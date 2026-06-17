ab5bdb8e6e1c82780b78a964dcf64bdf8fbde9e3
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-06-18 01:03:14 +0530

    Update Shortest Common Supersequence.cpp

--- DP/LCS/Shortest Common Supersequence.cpp
+++ DP/LCS/Shortest Common Supersequence.cpp
@@ -1,14 +1,19 @@
-summary-> text1+text2-LCS of text1 and text2
-
 class Solution {
 public:
-    string shortestCommonSupersequence(string text1, string text2) {
-         int n = text1.size();
-        int m = text2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(text1[i-1]==text2[j-1]){
+/*
+
+st1+str2-LCS
+
+*/
+    string shortestCommonSupersequence(string str1, string str2) {
+
+        int n=str1.size(),m=str2.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(str1[i-1]==str2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
@@ -16,36 +21,71 @@ public:
                 }
             }
         }
-          string scs="";
+
+        int lcs_length=dp[n][m];
+
+        cout<<lcs_length<<endl;
+
+        string LCS="";
+
         int i=n,j=m;
-        while(i>0 && j>0){
-            if(text1[i-1]==text2[j-1]){
-                scs+=text1[i-1];
-                i--; 
-              j--;
+
+        while(i>0 and j>0){
+
+            if(str1[i-1]==str2[j-1]){
+
+                LCS.push_back(str1[i-1]);
+                
+                i--;
+                j--;
             }
+
             else if(dp[i-1][j]>dp[i][j-1]){
-                scs+=text1[i-1];
                 i--;
             }
             else{
-                scs+= text2[j-1];
                 j--;
             }
         }
 
-        while(i>0){
-            scs+=text1[i-1];
-             i--;
+        reverse(LCS.begin(),LCS.end());
+
+        cout<<LCS<<endl;
+
+        string ans="";
+         
+         i=0,j=0;
+
+        for(auto c:LCS){
+
+            //jab str1 aur str2 ke character lcs se match nhi horhe tab tk add krdo
+            
+            while(str1[i]!=c){
+             ans.push_back(str1[i]);
+             i++;
+            }
+
+            
+
+            while(str2[j]!=c){
+             ans.push_back(str2[j]);
+             j++;
+            }
+
+            ans+=str1[i];  //abh jab dono same hai aur so add once
+
+            i++; //ek bar bdha do dono ko
+            j++; 
         }
-        while(j>0) {
-            scs+=text2[j-1];
-         j--;
-         }
 
-        reverse(scs.begin(), scs.end());
-        return scs;
+        //bache hue character add krdo
+
+        while(i<str1.size()){ ans.push_back(str1[i]); i++; }
+
+        while(j<str2.size()){ ans.push_back(str2[j]); j++; }
+
+
+        return ans;
+        
     }
-
-    
 };
