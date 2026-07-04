88bc3e2b8d5203e580b8462961a7424a32bbec9b
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-07-04 22:05:30 +0530

    Update Diameter of Binary Tree.cpp

--- Tree/Diameter of Binary Tree.cpp
+++ Tree/Diameter of Binary Tree.cpp
@@ -1,3 +1,32 @@
+Better approach->
+
+class Solution {
+public:
+int ans=0;
+int height(TreeNode* root){
+
+    if(!root) return 0;
+    int left=height(root->left);
+    int right=height(root->right);
+    ans=max(ans,left+right);
+
+    return 1+max(left,right);
+}
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+
+        return ans;
+    }
+};
+
+/*
+
+TC-> O(n)
+SC-> O(h)
+*/
+
+
+Brute force->
 /*
 
 Basically ham har node pr jaege aur uske left subtree ke max depth nikalege aur right subtree 
