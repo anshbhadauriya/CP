07c724b423b5e92a99750907e64c179ccbfa5f87
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-07-15 23:29:39 +0530

    Add files via upload

---
+++ DP/LIS/LIS using Binary Search/code.cpp
@@ -0,0 +1,32 @@
+/*
+now lets try binary search apporach
+its not intuitive
+*/
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+
+        vector<int>temp;
+        temp.push_back(nums[0]);
+
+        for(int i=1;i<nums.size();i++){
+
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+            }
+            else{
+
+                int idx=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[idx]=nums[i];
+            }
+        }
+
+        return temp.size();
+        
+    }
+};
+/*
+TC-> O(n logn)
+Sc-> O(n)
+*/
