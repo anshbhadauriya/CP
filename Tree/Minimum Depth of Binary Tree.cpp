065733c2a0d8141c9932e03fdca623d13596953b
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-08-30 10:31:25 +0530

    Create Minimum Depth of Binary Tree.cpp

---
+++ Tree/Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+
+        if(!root->left and !root->right) {
+            return 1;
+        }
+
+        if(!root->left){
+            return 1+minDepth(root->right);
+        }
+
+        if(!root->right){
+            return 1+minDepth(root->left);
+        }
+
+        return 1 + min(minDepth(root->left), minDepth(root->right));
+    }
+};
